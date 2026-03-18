ba71b7798206f261f102db868f426a5059da73b7
s35404 <s35404@pjwstk.edu.pl>
2026-03-18 11:14:36 +0100

    Init

---
+++ src/Ksiazka.java
@@ -0,0 +1,21 @@
+public class Ksiazka {
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
+
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+this.tytul=tytul;
+this.autor=autor;
+this.liczbaStron=liczbaStron;
+this.dostepna=dostepna;
+
+    }
+    public void println() {
+        System.out.println("Tytul: " + this.tytul);
+        System.out.println("Autor: " + this.autor);
+        System.out.println("Liczba Stron: " + this.liczbaStron);
+        System.out.println("Dostępna: " + this.dostepna);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Ksiazka bloodMeridian=new Ksiazka("Blood Meridian", "Cormac Mccarthy", 350, true );
+        printInfo(bloodMeridian);
+    }
+}
+
+
+
